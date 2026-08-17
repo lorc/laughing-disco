39ccfabe099d5bf1160756106bdf4497f1b3125f
Ruslan Ruslichenko <Ruslan_Ruslichenko@epam.com>
2026-08-17 10:42:49 +0200

    dwarf: prevent expr locator to consume ranges from other expr
    When rewinding to find 'is_stmt' for current expression do
    now allow to consume code ranges which belong to other
    expressions.
    Also added the test for this case.
    
    Signed-off-by: Ruslan Ruslichenko <Ruslan_Ruslichenko@epam.com>

--- tests/test_locations.c
+++ tests/test_locations.c
@@ -6,6 +6,7 @@ struct dt_device_match {
     const char *type;
     const char *compatible;
     const bool not_available;
+    int num;
     /*
      * Property name to search for. We only search for the property's
      * existence.
@@ -16,8 +17,18 @@ struct dt_device_match {
 
 #define macro1(x) (((x)->data == 0))
 #define macro2(x) (!!x)
+#define macro3 (g_matches.num == 2)
+#define macro4 (g_matches.num >= 1)
 
 #define MIN(x, y) ((x) < (y) ? (x) : (y))
+
+int test_func(int a)
+{
+	if (a)
+		return 1;
+
+	return 0;
+}
 
 int main(void)
 {
@@ -39,6 +50,8 @@ int main(void)
 	    return 0;
 
 	a = MIN(b, c);
+
+	test_func(macro3 ? 23 : macro4 ? 123 : 1234);
 
 	return 0;
 }
